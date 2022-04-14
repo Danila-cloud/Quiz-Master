2901a9fe2239d5ab43d505af3bc91df14a6e12ca
Danya Provilskyi <65830545+Danila-cloud@users.noreply.github.com>
2022-04-15 00:41:45 +0200

    add new methods

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,13 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+    //int correctAnswerIndex;
     void Start()
+    {
+        //DisplayQuestion();
+        GetNextQuestion();
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        if(index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!!!";
+            Image buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            string correctAnswer = question.GetAnswer(question.GetCorrectAnswerIndex());
+            questionText.text = "Sorry, the correct answer was " + correctAnswer;
+            Image buttonImage = answerButtons[question.GetCorrectAnswerIndex()].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        SetButtonState(false);
+    }
+    void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+    }
+    void SetDefaultButtonSprites()
+    {
+        for(int i = 0; i< answerButtons.Length; i++)
+        {
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
+        }
+
+    }
+    void DisplayQuestion()
     {
         questionText.text = question.GetQuestion();
 
@@ -18,5 +60,12 @@ public class Quiz : MonoBehaviour
             buttonText.text = question.GetAnswer(i);
         }
     }
-
+    void SetButtonState(bool state)
+    {
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
 }
